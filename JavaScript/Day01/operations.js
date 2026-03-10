101fffb912a64fce261bd130e7eade7bb206799e
shahariarshawon <shahariararafat7739@gmail.com>
2026-03-10 10:06:07 +0600

    Operations part has been revised

---
+++ JavaScript/Day01/operations.js
@@ -0,0 +1,10 @@
+const value = 3;
+let changedValue = value ** value
+
+console.log(changedValue);
+console.log("1"+2+2)
+console.log("1"+2+"2")
+console.log(1+2+"2")
+console.log(true + "shawon")
+
+// 1st ei j data type pabe sheta dhorei agabe shamne
