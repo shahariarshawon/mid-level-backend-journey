28399d70ca8b28afa696f9e066ad0eb6df02b334
shahariarshawon <shahariararafat7739@gmail.com>
2026-03-10 08:59:13 +0600

    Javascript data types revised

---
+++ JavaScript/Day01/index.js
@@ -0,0 +1,8 @@
+console.log(typeof"Shawon");
+/*There are several types of variable we can declare in the js. Such as-
+1. int
+2. biginit (ususally use for trade market web app)
+3. string 
+4. null er mane holo also a value but not 0 (typeof(null)= object)
+5. undefined er mane holo value ekhono assign kora hoynai (typeof(undefined)= undefined)
+*/
